b0983475c1d59669fc72c1724d42065cb176c0c6
gentili <gentili@8c7f07c7-4e7d-0410-9ed6-aab42a9a149d>
2013-09-25 03:01:51 +0000

    Get release script working for GameClient
    git-svn-id: svn://mcpnet.ca/demurrage/GameClient@640 8c7f07c7-4e7d-0410-9ed6-aab42a9a149d

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,1 +1,5 @@
-echo This is a shell script to install the client ${version}
+echo Installing ${releaseName} to videogamez.ca...
+echo ${project.version} > version.txt
+echo ${releaseName} > current.txt
+scp *.txt gentili@mcpnet.ca:videogamez.ca/htdocs/demurrage/GameClient
+scp ../${releaseName} gentili@mcpnet.ca:videogamez.ca/htdocs/demurrage/GameClient
