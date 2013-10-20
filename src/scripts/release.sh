55d8a1eea357a927077c971180cadfb1e716fee9
gentili <gentili@8c7f07c7-4e7d-0410-9ed6-aab42a9a149d>
2013-10-20 19:46:00 +0000

    Break GameLauncher into it's own build, with its own release scripts.
    git-svn-id: svn://mcpnet.ca/demurrage/GameClient@688 8c7f07c7-4e7d-0410-9ed6-aab42a9a149d

--- src/scripts/release.sh
+++ src/scripts/release.sh
@@ -1,4 +1,4 @@
+export TARGET="/home/gentili/videogamez.ca/htdocs/demurrage/GameClient/"
 echo Installing ${releaseName} to videogamez.ca...
-scp *.TXT gentili@mcpnet.ca:videogamez.ca/htdocs/demurrage/GameClient
-scp ../${releaseName} gentili@mcpnet.ca:videogamez.ca/htdocs/demurrage/GameClient
-scp GameLauncher.jar gentili@mcpnet.ca:videogamez.ca/htdocs/demurrage/GameLauncher/
+scp *.TXT gentili@mcpnet.ca:${TARGET}
+scp ../${releaseName} gentili@mcpnet.ca:${TARGET}
